cf77dc6843e3dfdb63900ce72982ec9467075d4f
zacky77-git <miyazaki.ytmym@me.com>
2021-01-05 22:26:48 +0900

    サイドバーに予定と日記を表示

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  before_action :set_events_diaries
+  
   private
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
+
+  def set_events_diaries
+    @index_events = Event.where('start_time >= ?', Date.today).order('start_time ASC').limit(5)
+    @index_diaries = Diary.where('start_time <= ?', Date.today).order('start_time ASC').limit(5)
+    end
 end
--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,20 +1,22 @@
 class TopController < ApplicationController
-  def index
-    # @monthly_targets = MonthlyTarget.where(date: "2021-01-05")
+  before_action :set_events_diaries
 
-    # @monthly_target = MonthlyTarget.where(user_id: :current_user.id )
+  def index
+
     @monthly_target = MonthlyTarget.where(user_id: current_user.id)
-    # binding.pry
     @weekly_target = WeeklyTarget.where(user_id: current_user.id)
     @daily_target = DailyTarget.where(user_id: current_user.id)
     @events = Event.where(user_id: current_user.id)
- 
-    # @monthly_target = MonthlyTarget.all
-    # @weekly_target = WeeklyTarget.all
-    # @daily_target = DailyTarget.all
-    # @events = Event.all
     @index_events = Event.where('start_time >= ?', Date.today).order('start_time ASC').limit(5)
     @index_diaries = Diary.where('start_time <= ?', Date.today).order('start_time ASC').limit(5)
     @tasks = current_user.tasks
   end
+
+  private
+
+  def set_events_diaries
+  @index_events = Event.where('start_time >= ?', Date.today).order('start_time ASC').limit(5)
+  @index_diaries = Diary.where('start_time <= ?', Date.today).order('start_time ASC').limit(5)
+  end
+
 end
